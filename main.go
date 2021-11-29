40bf03ffd79ba6af91debb6d977c370f36708c8f
mateo.perrotnasi@ynov.com <mateo.perrotnasi@ynov.com>
2021-11-29 20:44:35 +0100

    FIXED hangman position ADD tried variable that storage word and letter already used

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ type Hangman struct {
 	hiddenWord []string
 	attempt int
 	position string
+	tried []string
 }
 
 func main() {
@@ -23,16 +24,16 @@ func main() {
 	h1.CreateHidden()
 	h1.Reveal()
 	for i:= 0; i < 9999; i++ {
-		h1.PlayerTurn()
 		h1.HangmanPositions()
-		h1.Win()
-		h1.Loose()
+		h1.IsWin()
+		h1.IsLoose()
+		h1.PlayerTurn()
 	}
 }
 
 func (h *Hangman) ReadFile() {
 	rand.Seed(time.Now().UnixNano())
-	n := rand.Intn(37)
+	n := rand.Intn(38)
 	data, err := ioutil.ReadFile("words.txt")
 	content := string(data)
 	words := strings.Split(content, "\n")
@@ -40,15 +41,15 @@ func (h *Hangman) ReadFile() {
 		fmt.Println(err)
 
 	} else {
-		for i := 0; i < n-1; i++ {
+		for i := 0; i < n; i++ {
 			h.word = words[i]
 		}
 	}
-	fmt.Println(h.word)
+	// fmt.Println(h.word) // c'est le cheat code pour voir le mot
 }
 
 func (h *Hangman) CreateHidden() {
-	for i :=0; i < len(h.word); i++ {
+	for i := 0; i < len(h.word); i++ {
 		h.hiddenWord = append(h.hiddenWord, "_")
 	}
 }
@@ -60,20 +61,18 @@ func (h *Hangman) Reveal() {
 		for i := 1 ; i <= n ; i ++ {
 			randomLetter = rand.Intn(len(h.word))
 			for i, letter := range h.word {
-				if i == randomLetter {
+				if i == randomLetter && h.hiddenWord[i] == "_" {
 					h.hiddenWord[i] = string(letter)
-					fmt.Println(h.hiddenWord)
 				}
 			}
 		}
 	}
-	fmt.Println("Voici le mot à deviner :",h.hiddenWord)
 }
 
 func (h *Hangman) HangmanPositions() {
 	data, err := ioutil.ReadFile("hangman.txt")
 	content := string(data)
-	positions := strings.Split(content, "\n")
+	positions := strings.Split(content, "\n\n")
 	var n int = 10-h.attempt
 
 	if err != nil {
@@ -83,44 +82,57 @@ func (h *Hangman) HangmanPositions() {
 		for i := 0 ; i < n ; i++ {
 			h.position = positions[i]
 		}
-		fmt.Println(positions[n])
+		if n > 0 {
+			fmt.Println(positions[n-1])
+		}
 	}
 
 }
 
-func (h *Hangman) PlayerTurn() {	
+func (h *Hangman) PlayerTurn() {
 	var s string
 	found := false
+	fmt.Println("Voici le mot à deviner :",h.hiddenWord)
+	if h.attempt > 1 {
+		fmt.Println("Il vous reste", h.attempt ,"essais.")
+	} else {
+		fmt.Println("Il vous reste", h.attempt ,"essai.")
+	}
+	if h.tried != nil {
+		fmt.Println("Vous avez déjà essayé:",h.tried)
+	}
 	fmt.Println("Entrez un mot ou une lettre.")
 	fmt.Scanln(&s)
-	if len(s) > 0 {
-		for _, letter := range s {
-			if (letter < 'a' && letter > 'z') || (letter < 'A' && letter > 'Z') {
-				fmt.Println("Vous devez entrez une lettre")
+	strings.ToLower(s)
+	if len(s) > 1 {
+		if s == h.word {
+			for i, letter := range h.word {
+				h.hiddenWord[i] = string(letter)
+			}
+			h.IsWin()
+		} else {
+			fmt.Println("Ce n'était pas le bon mot.")
+			h.tried = append(h.tried, s)
+			h.attempt -= 1
+		}
+	} else {
+		for i, letter := range h.word {
+			if s == string(letter) {
+				h.hiddenWord[i] = string(letter) 
+				found = true
 			}
 		}
-	} else {
-		fmt.Println("Vous ne devez entrez qu'une seule lettre")
-	}
-	for i, letter := range h.word {
-		if s == string(letter) {
-			h.hiddenWord[i] = string(letter) 
-			found = true
+		if !found {
+			fmt.Println("Votre lettre n'est pas présente dans le mot.")
+			h.tried = append(h.tried, s)
+			h.attempt -= 1
+		} else {
+			fmt.Println("Bravo, vous avez deviné une lettre !")
 		}
-	}
-	if !found {
-		fmt.Println("Votre lettre n'est pas présente dans le mot.")
-		h.attempt -= 1
-		fmt.Println("Il vous reste", h.attempt,"tentatives")
-		h.HangmanPositions()
-		fmt.Println(h.hiddenWord)
-	} else {
-		fmt.Println("Bravo, vous avez deviné une lettre !")
-		fmt.Println(h.hiddenWord)
 	}
 }
 
-func (h *Hangman) Win() {
+func (h *Hangman) IsWin() {
 	match := false
 	Loop:
 	for i, letter := range h.word {
@@ -132,17 +144,15 @@ func (h *Hangman) Win() {
 		}
 	}
 	if match {
-		fmt.Println("Bravo, tu as gagnés ! le mot était bel est bien :", h.word)
+		fmt.Println("Bravo, tu as gagnés ! le mot était bel est bien", h.word)
 		os.Exit(0)
 	}
 }
 
-func (h *Hangman) Loose() {
+func (h *Hangman) IsLoose() {
 	if h.attempt == 0 {
 		fmt.Println("Tu as perdu, tes chances sont épuisés et José est mort.")
+		fmt.Println("Le mot a deviner était:", h.word)
 		os.Exit(0)	
 	}
 }
-
-
-// Le hangmanposition ne fonctionne pas.
